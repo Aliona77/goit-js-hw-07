df1826242167078a0a3e780e35fea6f922daba84
Aliona77 <chabanalona153@gmail.com>
2021-04-04 00:06:54 +0300

    зробила 5 завдання

---
+++ homework-07/src/js/task-05.js
@@ -0,0 +1,16 @@
+const nameInput = document.querySelector("#name-input");
+console.log(nameInput);
+
+const nameOutput = document.querySelector("#name-output");
+console.log(nameOutput);
+
+nameInput.addEventListener('input', hendleInputValue);
+
+function hendleInputValue(event) {
+    if (nameInput.value === '') {
+        nameOutput.textContent = 'незнакомец';
+    }
+    else {
+        nameOutput.textContent = nameInput.value;
+    }
+}
